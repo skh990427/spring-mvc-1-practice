5734e347f7044fc81ea542eb286fefa66c42bb6a
송광호 <skh990427@naver.com>
2024-12-18 16:56:08 +0900

    HTTP 요청 - 기본 헤더 조회

---
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -0,0 +1,39 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@Slf4j
+@RestController
+public class RequestHeaderController {
+
+    @RequestMapping("/headers")
+    public String headers(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            HttpMethod httpMethod,
+            Locale locale, //언어 정보
+            @RequestHeader MultiValueMap<String, String> headerMap, //헤더를 다받는거
+            @RequestHeader(name = "host") String host, //헤더를 하나만 받는, host = 필수 헤더
+            @CookieValue(value = "myCookie", required = false) String cookie // value = 쿠키이름
+    ) {
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+
+        return "ok";
+    }
+}
